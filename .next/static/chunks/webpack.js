5585cb6cdc757339d584b51a8a9aa60db453cbfd
Felipe <stephanovallejom@gmail.com>
2022-10-26 22:03:42 -0500

    Static generation and data fetching achieved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f21f1a9cb3bd206"; }
+/******/ 		__webpack_require__.h = function() { return "0333916789add6b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
