d9495e8066e85f07554f917fa2675f89b050b8e3
Felipe <stephanovallejom@gmail.com>
2022-10-26 19:48:18 -0500

    Nextjs13 is driving me crazy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+
+
 
 export default function Home() {
   return (
