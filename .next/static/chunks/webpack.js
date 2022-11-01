f4c25eded7a96a9bffd38ec09aa63fbe76eb615c
Felipe <stephanovallejom@gmail.com>
2022-10-31 22:52:44 -0500

    Polishing the post page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebb32361a4aa5904"; }
+/******/ 		__webpack_require__.h = function() { return "c88809dc78633b45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
