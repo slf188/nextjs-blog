c1c1a579cc6ece04616e6b85ccd8806b37f82992
Felipe <stephanovallejom@gmail.com>
2022-10-31 22:43:04 -0500

    Rendering markdown with remark

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e073955aacdc57e"; }
+/******/ 		__webpack_require__.h = function() { return "ebb32361a4aa5904"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
